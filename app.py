aa7a7b34ee75d28a680eca91120f2af175afc099
progokul20233 <progokul20233@gmail.com>
2026-02-15 22:10:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,6 +92,8 @@ def vote(player_id):
     conn.close()
     return redirect("/")
     
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
